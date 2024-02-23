3315955734be3b5ad6a49475821f3b6ea3ec05e7
KakarotisGoat <sahilrawat200506@gmail.com>
2024-02-23 21:38:59 +0530

    This is the 1st change

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import Navbar from './Navbar';
 import Footer from './Footer';
-// import Home from './Home';
-// import Manga from './Manga';
+import Home from './Home';
+import Manga from './Manga';
 import About from './About';
 import './App.css';
+// Always check the react-router-dom version before using and when used you have to make changes in index.js file also.
+// Routes -  acts as a container/parent for all the individual routes that will be created in our app.
+// Route -  is used to create a single route. 
+
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <>
       <Navbar />
+        <div className="App">
+        <Routes>
+          <Route path="/" element={ <Home/> } />
+          <Route path="manga" element={ <Manga/> } />
+          <Route path="about" element={ <About/> } />
+        </Routes>
+      </div>
       {/* <Home/> */}
       {/* <Manga/> */}
-      <About/>
       <Footer/>
     </>
   );
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav>
       <ul>
-        <li>Home</li>
-        <li>Manga</li>
-        <li>About</li>
+        <li><Link className="route" to="/">Home</Link></li>
+        <li><Link className="route" to="manga">Manga</Link></li>
+        <li><Link className="route" to="/about">About</Link></li>
       </ul>
     </nav>
   );
