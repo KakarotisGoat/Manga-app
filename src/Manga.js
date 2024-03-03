67be283c5cd4d40dd9333982a0a159d635f26173
KakarotisGoat <sahilrawat200506@gmail.com>
2024-03-03 16:47:13 +0530

    Allow tracking of image files

--- src/Manga.js
+++ src/Manga.js
@@ -2,17 +2,17 @@ import './App.css';
 import React from 'react';
 
 const Manga = () =>{
-    return(src/Components/
+    return(
         <section className='manga-section'>
-            <img className="manga-section-images" src="src/Components/Vagabond.jpg" alt="Vagabond-vol-1" />
-            <img className="manga-section-images" src="src/Components/Berserk.webp" alt="Berserk-vol-1" />
-            <img className="manga-section-images" src="src/Components/Demon_Slayer.jpg" alt="Demon_Slayer-vol-1" />
-            <img className="manga-section-images" src="src/Components/Chainsaw_man.webp" alt="Chainsaw_man-vol-1" />
-            <img className="manga-section-images" src="src/Components/Breaker.jpg" alt="Breaker-vol-1" />
-            <img className="manga-section-images" src="src/Components/JJK.jpg" alt="Jujutsu_Kaisen-vol-1" />
-            <img className="manga-section-images" src="src/Components/OPM.jpg" alt="One_Punch_Man-vol-1" />
-            <img className="manga-section-images" src="src/Components/ROR.webp" alt="Record_Of_Ragnarok-vol-1" />  
-            <img className="manga-section-images" src="src/Components/MHA.jpg" alt="My_Hero_Academia-vol-1" />
+            <img className="manga-section-images" src="Vagabond.jpg" alt="Vagabond-vol-1" />
+            <img className="manga-section-images" src="Berserk.webp" alt="Berserk-vol-1" />
+            <img className="manga-section-images" src="Demon_Slayer.jpg" alt="Demon_Slayer-vol-1" />
+            <img className="manga-section-images" src="Chainsaw_man.webp" alt="Chainsaw_man-vol-1" />
+            <img className="manga-section-images" src="Breaker.jpg" alt="Breaker-vol-1" />
+            <img className="manga-section-images" src="JJK.jpg" alt="Jujutsu_Kaisen-vol-1" />
+            <img className="manga-section-images" src="OPM.jpg" alt="One_Punch_Man-vol-1" />
+            <img className="manga-section-images" src="ROR.webp" alt="Record_Of_Ragnarok-vol-1" />  
+            <img className="manga-section-images" src="MHA.jpg" alt="My_Hero_Academia-vol-1" />
         </section>
     );
 };
